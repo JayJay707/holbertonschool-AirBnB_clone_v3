2a24aab2d8615a9ba4db8e22a8103e395188956d
JayJay707 <70692566+JayJay707@users.noreply.github.com>
2022-09-15 11:36:50 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+               def get(self, cls, id):
+           """Gets object"""
+           obj = self.__session.query(cls).get(id)
+           if obj == None:
+                      return None
+           return obj
+
+    def count(self, cls=None):
+           """Counts objects"""
+           objs = self.all(cls)
+           return len(objs)
