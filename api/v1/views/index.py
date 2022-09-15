a44973ce5724057d2e2976aa42c6cb9983557912
JayJay707 <70692566+JayJay707@users.noreply.github.com>
2022-09-15 12:22:39 +0200

    Added stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,16 @@ def status():
     """ Status of API """
     ok_status = {"status": "OK"}
     return jsonify(ok_status)
+
+@app_views.route('/stats')
+def stats():
+    """
+        return dict count of data
+    """
+    my_dict = {"amenities": storage.count(Amenity),
+               "cities": storage.count(City),
+               "places": storage.count(Place),
+               "reviews": storage.count(Review),
+               "states": storage.count(State),
+               "users": storage.count(User)}
+    return jsonify(my_dict)
